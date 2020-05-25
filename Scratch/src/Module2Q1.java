4f65df0548b0436af1df5c0f5e15202d8c44d14c
jgreen44 <jgreen44@asu.edu>
2020-05-25 16:00:16 -0500

    Module 2: Short Answer Q:1

---
+++ Scratch/src/Module2Q1.java
@@ -0,0 +1,13 @@
+public class Module2Q1 {
+
+
+    public double someDistance(Point2D points){
+        Point2D p = new Point2D(3, 5);
+        if (p.distanceTo(points) < 10){
+            System.out.println("The distance between" +
+                    p.toString() + " and " points.toString() +
+                    is p.distanceTo(points));
+
+        }
+    }
+}
